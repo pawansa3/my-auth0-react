34cee44c1c314ecc0dd88db54ac2ddbcf2a49722
Pawan Sah <pawan.sah@vemo.com>
2020-05-06 12:08:24 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# my-auth0-react
---
+++ src/index.js
@@ -0,0 +1,34 @@
+// src/index.js
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Auth0Provider } from "./react-auth0-spa";
+import config from "./auth_config.json";
+import history from "./utils/history";
+
+// A function that routes the user to the right place
+// after login
+const onRedirectCallback = appState => {
+  console.log('appstate',appState, window.location)
+  history.push(
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
+
+ReactDOM.render(
+  <Auth0Provider
+    domain={config.domain}
+    client_id={config.clientId}
+    redirect_uri={window.location.origin}
+    audience={config.audience}     // NEW - specify the audience value
+    onRedirectCallback={onRedirectCallback}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
+);
+
+// serviceWorker.unregister();
